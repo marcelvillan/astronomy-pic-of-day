e9c71e6e7ee082ad14c891f5368044c4faea1df7
Marcel <proxalidamine@gmail.com>
2022-04-01 14:17:58 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AstroListComponent } from './astro-list/astro-list.component';
+import { AstroDetailComponent } from './astro-detail/astro-detail.component';
+import { AstroItemComponent } from './astro-list/astro-item/astro-item.component';
+import { AstroService } from './astro.service';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AstroListComponent,
+    AstroDetailComponent,
+    AstroItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
   ],
-  providers: [],
+  providers: [AstroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
