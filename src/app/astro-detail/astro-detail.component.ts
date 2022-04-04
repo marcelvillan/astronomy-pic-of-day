2e1147a1047853c4f522de56255cffcd0c5a402a
marcelvillan <proxalidamine@gmail.com>
2022-04-04 17:27:24 +0200

    Updated styles

--- src/app/astro-detail/astro-detail.component.ts
+++ src/app/astro-detail/astro-detail.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
 import { Astro } from '../shared/astro.model';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-astro-detail',
@@ -9,16 +10,33 @@ import { Astro } from '../shared/astro.model';
 export class AstroDetailComponent implements OnInit {
 @Input() astroInput:Astro;
 @Output() closeButton = new EventEmitter<string>();
+@HostListener('window:resize', ['$event'])
+safeURL:any;
+screenHeight: number;
+screenWidth: number;
 astroItem:Astro;
-  constructor() {
+  constructor(private _sanitizer: DomSanitizer) {
+
+
+    this.getScreenSize();
   }
 
   ngOnInit(): void {
 this.astroItem=this.astroInput;
+this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl(this.astroItem.url);
   }
   closeDetail(){
 this.closeButton.emit();
   }
+  onResize(event?) {
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+ }
+ getScreenSize(event?) {
+  this.screenHeight = window.innerHeight;
+  this.screenWidth = window.innerWidth;
+  console.log(this.screenHeight, this.screenWidth);
+}
 
 
 }
